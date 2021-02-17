029fdb6906af3ce2808e6fadc740010be3c2c203
onyima101 <41713172+onyima101@users.noreply.github.com>
2021-02-17 16:52:47 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id                  = "${ aws_vpc.vpc_nd.id }"
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "us-east-2a"
 
   tags = {
     Name      = "Public Subnet"
@@ -27,7 +27,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = "${ aws_vpc.vpc_nd.id }"
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-2a"
 
   tags = {
     Name      = "Private Subnet"
